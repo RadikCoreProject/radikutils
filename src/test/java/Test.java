e689af15ccdf77ad4b370c1edcfccfa9f5dec8a1
radik <aleshiniwan09@gmail.com>
2025-08-29 10:33:09 +0500

    fix test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,13 +1,23 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Random;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 class Test {
     public static void main(String[] args) {
-        Random rand = new Random();
-        int a = 1;
-        Integer.
+        Scanner scanner = new Scanner(System.in);
+        String atoms = scanner.next(); // число в обертке строки
+        int H = scanner.nextInt();
+        int He = scanner.nextInt();
+        Double.POSITIVE_INFINITY
+
+        if (H + He > 100) {
+            System.out.print("Вселенная не существует");
+            return;
+        }
+        atoms.matches()
+
+        BigInteger atoms1 = new BigInteger(atoms);
+        BigInteger atoms1precent = atoms1.divide(new BigInteger("100"));
+        BigInteger ans = atoms1.multiply(new BigInteger(String.valueOf(100 - H - He)));
+        System.out.println(ans);
     }
 }
---
+++ src/test/java/tetetete.java
@@ -0,0 +1,8 @@
+public enum tetetete {
+    A (1),
+    B (2),
+    C (3);
+
+    tetetete(int i) {
+    }
+}
